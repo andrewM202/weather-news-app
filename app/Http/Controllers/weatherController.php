0ce7d8be0908152bf3eac2c82097ac349c07d427
Andrew Matt <andrewm1246@gmail.com>
2021-09-04 14:14:07 -0400

    change api url to use https

--- app/Http/Controllers/weatherController.php
+++ app/Http/Controllers/weatherController.php
@@ -9,7 +9,7 @@ class weatherController extends Controller
     public function weatherData() {
         $city_name = 'Lancaster';
         $api_key = env('OPENWEATHER_API_KEY=');
-        $api_url = 'http://api.openweathermap.org/data/2.5/weather?&units=imperial&q='.$city_name.'&appid='.$api_key;
+        $api_url = 'https://api.openweathermap.org/data/2.5/weather?&units=imperial&q='.$city_name.'&appid='.$api_key;
         $weather_data = json_decode(file_get_contents($api_url), true);
 
         // return $temperature;
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+// Used to mess with URLs
 use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,8 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
+        // If we are on a production environment, generate a secure URL!
         if (env('APP_ENV') !== 'local') {
             $url->forceScheme('https');
-      }
+        }
     }
 }
