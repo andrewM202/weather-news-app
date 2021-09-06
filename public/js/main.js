41dbf4c8718a2738f652cca3e7e44a38746e9151
Andrew Matt <andrewm1246@gmail.com>
2021-09-06 14:52:14 -0400

    heroku test

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function()  {
+    console.log('test')
     $(window).on("resize", function() {
         height = $(".test").css("width");
         $(".test").css("min-height", height)
